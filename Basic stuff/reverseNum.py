f85ca7da711248fd1f015087fe3495ebb7686514
DuckDoesStuff <mailtienduc@gmail.com>
2022-05-09 11:59:22 +0700

    made a recursive function

--- Basic stuff/reverseNum.py
+++ Basic stuff/reverseNum.py
@@ -1,8 +1,21 @@
+def recur_reverse(num):#recursive method
+    global rev_num
+    if(num > 0):
+        remain = num % 10
+        rev_num = (rev_num) * 10 + remain
+        recur_reverse(num // 10)
+    return rev_num
+
+# num = int(input("Enter a number: "))
+# rev_num = 0
+# rmder = 0
+# while(num > 0):
+#     rmder = num % 10
+#     rev_num = rev_num*10 + rmder
+#     num = num // 10
+# print(str(rev_num))
+
 num = int(input("Enter a number: "))
 rev_num = 0
-rmder = 0
-while(num > 0):
-    rmder = num % 10
-    rev_num = rev_num*10 + rmder
-    num = num // 10
+rev_num = recur_reverse(num)
 print(str(rev_num))
