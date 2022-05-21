4c6eec7217367eddcfedb87ba497805fb8836f90
DuckDoesStuff <mailtienduc@gmail.com>
2022-05-21 18:00:20 +0700

    leetcode 206

---
+++ Python/reverseLinkedList.py
@@ -0,0 +1,18 @@
+class ListNode:
+     def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        curr = head
+        prev = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr = nxt
+        
+        return prev
