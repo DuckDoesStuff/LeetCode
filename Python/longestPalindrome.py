68def3ee5f6910f8489057526ee345cdfd0ed815
DuckDoesStuff <mailtienduc@gmail.com>
2022-05-24 09:01:17 +0700

    Leetcode 5

---
+++ Python/longestPalindrome.py
@@ -0,0 +1,17 @@
+def longestPalindrome(self, s: str) -> str:
+        res = ""
+        
+        for i in range(0, len(s)):
+            l, r = i, i
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                if(r - l + 1 > len(res)):
+                    res = s[l:r+1];
+                l -= 1
+                r += 1
+            l, r = i, i + 1
+            while(l >= 0 and r < len(s) and s[l] == s[r]):
+                if(r - l + 1 > len(res)):
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+        return res
